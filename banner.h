cf7e5261b76d297b3a654fe41c5456155e0c2963
Barkeotullah Opu <146110521+Barkotullah02@users.noreply.github.com>
2023-11-05 21:54:29 +0600

    Used banner for software publicity.

---
+++ banner.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void banner(void);
+
+
+void banner(void){
+    printf("\t\t\t\t     Welcome to Barkotullah Website Vendor   \n");
+    printf("\t\t\t\t ___________________________________________ \n");
+    printf("\t\t\t\t|| HOME                 PORTFOLIO     MENU ||\n");
+    printf("\t\t\t\t||_________________________________________||\n");
+    printf("\t\t\t\t||        ************************         ||\n");
+    printf("\t\t\t\t||        * We offer you a FULLY *         ||\n");
+    printf("\t\t\t\t||        *  RESPONSIVE WEBSITE  *         ||\n");
+    printf("\t\t\t\t||        *    In any devices    *         ||\n");
+    printf("\t\t\t\t||        ************************         ||\n");
+    printf("\t\t\t\t||                                         ||\n");
+    printf("\t\t\t\t||_________________________________________||\n");
+    printf("\t\t\t\t||       FB INSTA TWITTER +8801566001546   ||\n");
+    printf("\t\t\t\t||_________________________________________||\n");
+}
+
+
